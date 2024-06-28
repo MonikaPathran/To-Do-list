4195e4c6166cbc5f2027ca6572b90a400c12bb56
Monika Pathran <119341907+MonikaPathran@users.noreply.github.com>
2024-06-28 11:45:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainApp from './MainApp';
+
+
+function App() {
+  return (
+    <React.Fragment>
+        <MainApp />
+    </React.Fragment>
+      
+  );
+}
+
+export default App;
